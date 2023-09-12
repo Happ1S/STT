6d673c56aa21b22ea590675ee791237b35e4e613
Happ1S <fhappis@gmail.com>
2023-09-12 22:23:25 +0300

    stable v1.

---
+++ main.py
@@ -0,0 +1,12 @@
+from record_to_file import record
+from voice_recognizer import recognizer
+
+def main():
+    record()
+    recognizer()
+    input()
+
+
+if __name__ == '__main__':
+    main()
+    
